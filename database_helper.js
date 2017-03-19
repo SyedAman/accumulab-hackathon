7c170f2f12b319791ed1efbd9080fdb1660aef90
Syed Aman <sna274@nyu.edu>
2017-03-19 05:27:08 -0500

    use cakebaker as boilerplate

--- database_helper.js
+++ database_helper.js
@@ -1,47 +1,38 @@
 'use strict';
 module.change_code = 1;
 var _ = require('lodash');
-var SCRYVR_DATA_TABLE_NAME = 'scryvr';
+var CAKEBAKER_DATA_TABLE_NAME = 'cakeBakerData';
 var dynasty = require('dynasty')({});
 
-// var localDynasty = require('dynasty')(localCredentials, localUrl);
-// var localUrl = 'http://localhost:8000';
-// var localCredentials = {
-//   region: 'us-east-1',
-//   accessKeyId: 'fake',
-//   secretAccessKey: 'fake'
-// };
-// var localDynasty = require('dynasty')(localCredentials, localUrl);
-// var dynasty = localDynasty;
-
-function Scryvr() {}
-var scryvrTable = function() {
-  return dynasty.table(SCRYVR_DATA_TABLE_NAME);
+function CakeBakerHelper() {}
+var cakeBakerTable = function() {
+  return dynasty.table(CAKEBAKER_DATA_TABLE_NAME);
 };
 
-Scryvr.prototype.createScryvrTable = function() {
-  return dynasty.describe(SCRYVR_DATA_TABLE_NAME)
+CakeBakerHelper.prototype.createCakeBakerTable = function() {
+  return dynasty.describe(CAKEBAKER_DATA_TABLE_NAME)
     .catch(function(error) {
-      return dynasty.create(SCRYVR_DATA_TABLE_NAME, {
+      return dynasty.create(CAKEBAKER_DATA_TABLE_NAME, {
         key_schema: {
           hash: ['userId',
-           'string']
+            'string'
+          ]
         }
       });
     });
 };
 
-Scryvr.prototype.storeScryvrData = function(userId, scryvrData) {
-  return scryvrTable().insert({
+CakeBakerHelper.prototype.storeCakeBakerData = function(userId, cakeBakerData) {
+  return cakeBakerTable().insert({
     userId: userId,
-    data: scryvrData
+    data: cakeBakerData
   }).catch(function(error) {
     console.log(error);
   });
 };
 
-Scryvr.prototype.readScryvrData = function(userId) {
-  return scryvrTable().find(userId)
+CakeBakerHelper.prototype.readCakeBakerData = function(userId) {
+  return cakeBakerTable().find(userId)
     .then(function(result) {
       return result;
     })
@@ -50,4 +41,4 @@ Scryvr.prototype.readScryvrData = function(userId) {
     });
 };
 
-module.exports = Scryvr;
+module.exports = CakeBakerHelper;
--- index.js
+++ index.js
@@ -2,37 +2,37 @@
 module.change_code = 1;
 var _ = require('lodash');
 var Skill = require('alexa-app');
-var SCRYVR_SESSION_KEY = 'scryvr';
-var skillService = new Skill.app('scryvr');
-var Scryvr = require('./scryvr');
+var CAKE_BAKER_SESSION_KEY = 'cake_baker';
+var skillService = new Skill.app('cakebaker');
+var CakeBakerHelper = require('./cakebaker_helper');
 var DatabaseHelper = require('./database_helper');
 var databaseHelper = new DatabaseHelper();
+
 skillService.pre = function(request, response, type) {
-  databaseHelper.createScryvrTable();
+  databaseHelper.createCakeBakerTable();
 };
-var getScryvr = function(scryvrData) {
-  if (scryvrData === undefined) {
-    scryvrData = {};
+var getCakeBakerHelper = function(cakeBakerHelperData) {
+  if (cakeBakerHelperData === undefined) {
+    cakeBakerHelperData = {};
   }
-  return new Scryvr(scryvrData);
+  return new CakeBakerHelper(cakeBakerHelperData);
+};
+var getCakeBakerHelperFromRequest = function(request) {
+  var cakeBakerHelperData = request.session(CAKE_BAKER_SESSION_KEY);
+  return getCakeBakerHelper(cakeBakerHelperData);
 };
 
-var getScryvrFromRequest = function(request) {
-  var scryvrData = request.session(SCRVYR_SESSION_KEY);
-  return getScryvr(scryvrData);
-};
-
-var scryvrIntentFunction = function(scryvr, request, response) {
-  console.log(scryvr);
-  if (scryvr.completed()) {
-    response.say('End of procedure. Scryvr will periodically follow up with you to check on the status of your symptoms.');
+var cakeBakerIntentFunction = function(cakeBakerHelper, request, response) {
+  console.log(cakeBakerHelper);
+  if (cakeBakerHelper.completed()) {
+    response.say('Congratulations! Your cake is complete!');
     response.shouldEndSession(true);
   } else {
-    response.say(Scryvr.getPrompt());
-    response.reprompt("I didnt hear you. " + scryvr.getPrompt());
+    response.say(cakeBakerHelper.getPrompt());
+    response.reprompt("I didnt hear you. " + cakeBakerHelper.getPrompt());
     response.shouldEndSession(false);
   }
-  response.session(SCRYVR_SESSION_KEY, scryvr);
+  response.session(CAKE_BAKER_SESSION_KEY, cakeBakerHelper);
   response.send();
 };
 
@@ -40,57 +40,68 @@ skillService.intent('advanceStepIntent', {
     'utterances': ['{next|advance|continue}']
   },
   function(request, response) {
-    var scryvr = getScryvrFromRequest(request);
-    scryvr.currentStep++;
-    saveTreatment(scryvr, request);
-    scryvrIntentFunction(scryvr, request, response);
+    var cakeBakerHelper = getCakeBakerHelperFromRequest(request);
+    cakeBakerHelper.currentStep++;
+    saveCake(cakeBakerHelper, request);
+    cakeBakerIntentFunction(cakeBakerHelper, request, response);
+  }
+);
+
+skillService.intent('repeatStepIntent', {
+    'utterances': ['{repeat}']
+  },
+  function(request, response) {
+    var cakeBakerHelper = getCakeBakerHelperFromRequest(request);
+    cakeBakerIntentFunction(cakeBakerHelper, request, response);
   }
 );
 
 skillService.launch(function(request, response) {
-  var prompt = 'Welcome to Accumulab! To start, say scryvr start the treatment';
+  var prompt = 'Welcome to Cakebaker!' +
+    'To start baking, say bake a cake';
   response.say(prompt).shouldEndSession(false);
 });
 
-skillService.intent('scryvrIntent', {
-    'utterances': ['{start} {the} treatment)']
+skillService.intent('cakeBakeIntent', {
+    'utterances': ['{new|start|create|begin|build} {|a|the} cake']
   },
   function(request, response) {
-    var scryvr = new Scryvr({});
-    scryvrIntentFunction(scryvr, request, response);
+    var cakeBakerHelper = new CakeBakerHelper({});
+    cakeBakerIntentFunction(cakeBakerHelper, request, response);
   }
 );
 
-skillService.intent('saveTreatmentIntent', {
-    'utterances': ['{save} {the|my} treatment']
-  },
-  function(request, response) {
-   var scryvr = getScryvrFromRequest(request);
-   saveTreatment(scryvr, request);
-   response.say('Treatment progress has been saved!');
-   response.shouldEndSession(true).send();
-   return false;
-  }
-);
-
-skillService.intent('loadTreatmentIntent', {
-    'utterances': ['{load|resume} {my} {|last} treatment']
+skillService.intent('loadCakeIntent', {
+    'utterances': ['{load|resume} {|a|the} {|last} cake']
   },
   function(request, response) {
     var userId = request.userId;
-       databaseHelper.readScryvrData(userId).then(function(result) {
-         return (result === undefined ? {} : JSON.parse(result['data']));
-       }).then(function(loadedScryvrData) {
-         var scryvr = new Scryvr(loadedScryvrData);
-         return scryvrIntentFunction(scryvr, request, response);
-       });
-       return false;
+    databaseHelper.readCakeBakerData(userId).then(function(result) {
+      return (result === undefined ? {} : JSON.parse(result['data']));
+    }).then(function(loadedCakeBakerData) {
+      var cakeBakerHelper = new CakeBakerHelper(loadedCakeBakerData);
+      return cakeBakerIntentFunction(cakeBakerHelper, request, response);
+    });
+    return false;
   }
 );
 
-var saveTreatment = function(scryvr, request) {
+skillService.intent('saveCakeIntent', {
+    'utterances': ['{save} {|a|the|my} cake']
+  },
+  function(request, response) {
+    var cakeBakerHelper = getCakeBakerHelperFromRequest(request);
+    saveCake(cakeBakerHelper, request);
+    response.say('Your cake progress has been saved!');
+    response.shouldEndSession(true).send();
+    return false;
+  }
+);
+
+var saveCake = function(cakeBakerHelper, request) {
   var userId = request.userId;
-  databaseHelper.storeScryvrData(userId, JSON.stringify(scryvr))
+  databaseHelper.storeCakeBakerData(userId, JSON.stringify(
+      cakeBakerHelper))
     .then(function(result) {
       return result;
     }).catch(function(error) {
